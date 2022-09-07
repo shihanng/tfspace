3f8cae39cf629c002aaf2051c15d47d1f03376a1
Shi Han NG <shihanng@gmail.com>
2022-09-07 23:53:34 +0900

    store: Include test coverage on Load

--- store/store_test.go
+++ store/store_test.go
@@ -28,3 +28,31 @@ func TestLoad(t *testing.T) {
 	assert.NilError(t, err)
 	assert.DeepEqual(t, actual, expected)
 }
+
+func TestLoad_BadYML(t *testing.T) {
+	actual, err := store.Load("./testdata/load_bad_yml.yml")
+
+	assert.ErrorContains(t, err, "source data must be an array or slice")
+	assert.Assert(t, actual == nil)
+}
+
+func TestLoad_IntKey(t *testing.T) {
+	actual, err := store.Load("./testdata/load_int_key.yml")
+
+	assert.ErrorContains(t, err, "name is not string")
+	assert.Assert(t, actual == nil)
+}
+
+func TestLoad_NotHash(t *testing.T) {
+	actual, err := store.Load("./testdata/load_not_hash.yml")
+
+	assert.ErrorContains(t, err, "value is not hash map")
+	assert.Assert(t, actual == nil)
+}
+
+func TestLoad_Text(t *testing.T) {
+	actual, err := store.Load("./testdata/load_text.yml")
+
+	assert.ErrorContains(t, err, "string was used where mapping")
+	assert.Assert(t, actual == nil)
+}
