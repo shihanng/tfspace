60d447603be36bec19589e0440d0d85d2260f65b
Shi Han NG <shihanng@gmail.com>
2022-09-10 12:25:38 +0900

    Introduce options to cmd.Execute

--- cmd/root.go
+++ cmd/root.go
@@ -2,21 +2,28 @@
 package cmd
 
 import (
-	"os"
-
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{ //nolint:exhaustruct,gochecknoglobals
-	SilenceUsage: true,
-	Use:          "tfspace",
-	Short:        "Manage multiple environments in a Terraform project with ease.",
-	Long:         "Manage multiple environments in a Terraform project with ease.",
+// Execute is the entrypoint to tfspace root command.
+func Execute(options ...func(*cobra.Command)) {
+	rootCmd := &cobra.Command{ //nolint:exhaustruct
+		SilenceUsage: true,
+		Use:          "tfspace",
+		Short:        "Manage multiple environments in a Terraform project with ease.",
+		Long:         "Manage multiple environments in a Terraform project with ease.",
+	}
+
+	for _, option := range options {
+		option(rootCmd)
+	}
+
+	cobra.CheckErr(rootCmd.Execute())
 }
 
-// Execute is the entrypoint to tfspace root command.
-func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		os.Exit(1)
+// WithArgs pass arguments to root command. This is for testing purpose.
+func WithArgs(args ...string) func(*cobra.Command) {
+	return func(c *cobra.Command) {
+		c.SetArgs(args)
 	}
 }
