1f8c782fbe32d2579d0e5aaeb6d4cc00fbef8fe1
Shi Han NG <shihanng@gmail.com>
2022-11-02 21:02:55 +0900

    Remove flag package

--- config/config.go
+++ config/config.go
@@ -3,7 +3,6 @@ package config
 
 import (
 	"github.com/cockroachdb/errors"
-	"github.com/shihanng/tfspace/flag"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -15,7 +14,7 @@ type Config struct {
 
 // WithConfig add Config related flags to the command.
 func WithConfig(cmd *cobra.Command) {
-	flag.StringP(cmd.PersistentFlags(), "config", "c", "./tfspace.yml", "path to tfspace.yml file")
+	cmd.PersistentFlags().StringP("config", "c", "./tfspace.yml", "path to tfspace.yml file")
 }
 
 // GetConfig collects all values from flags and return Config with those values.
